27f8e66528d68b633c2425f499202a34f8d3a246
Eric <developer.ericgitangu@gmail.com>
2023-12-14 16:39:06 -0600

    Code refactor.

--- app/src/main/java/com/example/littlelemon/HomeScreen.kt
+++ app/src/main/java/com/example/littlelemon/HomeScreen.kt
@@ -1,7 +1,7 @@
 package com.example.littlelemon
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
@@ -11,13 +11,10 @@ import androidx.navigation.NavHostController
 @ExperimentalMaterial3Api
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .fillMaxSize()
-    ) {
+    Column (Modifier.fillMaxWidth().fillMaxHeight()){
         TopAppBar()
         UpperPanel()
+        WeeklySpecialCard()
         LowerPanel(navController, DishRepository.dishes)
     }
 }
